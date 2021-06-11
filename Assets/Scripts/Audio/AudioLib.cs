5571fd3f9a6e0e28c768f13ffeb9ccd1332224ad
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-06-10 23:40:45 -0300

    Plataforma longa feita

--- Assets/Scripts/Audio/AudioLib.cs
+++ Assets/Scripts/Audio/AudioLib.cs
@@ -19,4 +19,16 @@ public class AudioLib : MonoBehaviour
     {
         _audioSource.PlayOneShot(Random.Range(0.0f, 1.0f) < 0.5 ? capa_impacto_grande_final : capa_transicao);
     } 
+    
+    public IEnumerator StopLightSFX(){
+        if (_audioSource.isPlaying)
+            yield return null;
+        yield return new WaitWhile(isPlaying);
+        _audioSource.Stop();
+    }
+
+    private bool isPlaying()
+    {
+        return _audioSource.volume > 0f;
+    }
 }
