22733bc9f6af1a71e1322c3dab0f38538f780803
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-06-14 00:25:46 -0300

    Torre A a caminho. Sistema de plataforma finalizado

--- Assets/Scripts/Interactions/Responders/ControlledPlatform.cs
+++ Assets/Scripts/Interactions/Responders/ControlledPlatform.cs
@@ -121,5 +121,7 @@ namespace Responders
             );
         }
         
+        
+        
     }
 }
--- Assets/Scripts/Structure/PlayerStandAtopPlatform.cs
+++ Assets/Scripts/Structure/PlayerStandAtopPlatform.cs
@@ -14,9 +14,23 @@ public class PlayerStandAtopPlatform : MonoBehaviour
     [SerializeField] private GameObject[] activables;
 
     private Transform _originalParent;
+    private PositionCheckingBox _checkingBox;
+    private Tuple<Vector2, Vector2> _checkingBoxOriginalData; //modificar depois
+    private bool hasPlayerAtop;
+    private float OriginalSizeBoxY;
+    private float OriginalOffsetBoxY;
 
 
+    private void Awake()
+    {
+        if (TryGetComponent(out _checkingBox))
+        {
+            OriginalSizeBoxY = _checkingBox.CollisionDetectorSize.y;
+            OriginalOffsetBoxY = _checkingBox.CollisionDetectorOffset.y;
 
+        }
+        
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -30,13 +44,21 @@ public class PlayerStandAtopPlatform : MonoBehaviour
             // Debug.Log("Hit the top: " + other.GetContact(0).normal);
             _originalParent = other.transform.parent;
             other.transform.SetParent(transform);
-            activables.Where(s => s && !s.activeSelf).ForEach(s => s.SetActive(true));
+            hasPlayerAtop = true;
+            _checkingBoxOriginalData = _checkingBox.AdaptToAboveEntity(
+                    _checkingBox.CollisionDetectorSize.x, 7,
+                    _checkingBox.CollisionDetectorOffset.x, 2);
+                activables.Where(s => s && !s.activeSelf).ForEach(s => s.SetActive(true));
         }
     }
 
 
     private void OnCollisionStay2D(Collision2D other)
     {
+        if (other.GetContact(0).normal.y < 0 == false)
+        {
+            return;
+        }
         activables.Where(s => s && !s.activeSelf).ForEach(s => s.SetActive(true));
     }
 
@@ -49,6 +71,17 @@ public class PlayerStandAtopPlatform : MonoBehaviour
         }
         other.transform.SetParent(_originalParent);
         activables.Where(s => s && s.activeSelf).ForEach(s => s.SetActive(false));
-
+        hasPlayerAtop = false;
+        if (_checkingBox != null)
+        {
+            _checkingBox.AdaptToAboveEntity(
+                _checkingBox.CollisionDetectorSize.x,  _checkingBoxOriginalData.Item1.y,
+                _checkingBox.CollisionDetectorOffset.x, _checkingBoxOriginalData.Item2.y); 
+            print(_checkingBox.CollisionDetectorSize);
+        }
     }
+    
+    
+
+    public bool HasPlayerAtop => hasPlayerAtop;
 }
