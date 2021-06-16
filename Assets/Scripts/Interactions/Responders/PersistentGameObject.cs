d3b7050f40da91cee80482f4c4d5faec24bb3e74
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-06-15 21:18:38 -0300

    Cena inicial finalizada; Próximo: finalizar cena pós-teleporte

---
+++ Assets/Scripts/Interactions/Responders/PersistentGameObject.cs
@@ -0,0 +1,15 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+namespace Responders
+{
+    public class PersistentGameObject : MonoCache
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            print("persist");
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
