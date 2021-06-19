ed639ea27c0288fcb768dd1541dbbfa881d5ba78
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-06-19 19:07:29 -0300

    Menu em modo básico pronto. Colocar post-processing e efeitos de luz

---
+++ Assets/Scripts/Aux Classes/SceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Aux_Classes
+{
+    public class SceneManager : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("FINAL");
+        }
+    }
+}
